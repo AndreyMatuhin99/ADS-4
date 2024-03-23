49e8ba5adb5204b021bfcf8128d92cd85c5ca458
AndreyMatuhin99 <116704246+AndreyMatuhin99@users.noreply.github.com>
2024-03-23 18:04:01 +0300

    Задание 4

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,66 +1,96 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int* arr, int size, int value) {
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int countPairs1(const std::vector<int>& arr, int value) {
     int count = 0;
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = i + 1; j < size; j++) {
+    int len = arr.size();
+    for (int i = 0; i < (len - 1); i++) {
+        for (int j = i + 1; j < len; j++) {
             if ((arr[i] + arr[j]) == value) {
-                count += 1;
+                count++;
             }
         }
     }
     return count;
 }
 
-int countPairs2(int* arr, int len, int value) {
-    int count = 0;
-    int left = 0;
-    int right = len - 1;
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-        if (sum == value) {
-            ++count;
-            ++left;
-            --right;
-        } else if (sum < value) {
-            ++left;
+int countPairs2(const std::vector<int>& arr, int value) {
+    int count = 0, x = 0, j = 0;
+    int len = arr.size();
+    for (int i = 0; i < (len - 1); i++) {
+        if ((arr[i] + arr[i + 1]) >= value) {
+            x = i;
+            break;
+        }
+    }
+    for (int i = 0; i < (len - 1); i++) {
+        if (arr[i] > value) {
+            break;
+        }
+        if (x <= i) {
+            j = i + 1;
         } else {
-            --right;
+            j = x;
+        }
+        for (; j < len; j++) {
+            if ((arr[i] + arr[j]) == value) {
+                count++;
+            }
+            if (arr[j] > value) {
+                break;
+            }
         }
     }
     return count;
 }
 
-int binarySearch(int* arr, int left, int right, int target) {
+int countPairs3(const std::vector<int>& arr, int value) {
     int count = 0;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == target) {
-            ++count;
-            int l = mid - 1;
-            int r = mid + 1;
-            while (l >= left && arr[l] == target) {
-                ++count;
-                --l;
-            }
-            while (r <= right && arr[r] == target) {
-                ++count;
-                ++r;
-            }
-            return count;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    return count;
-}
-
-int countPairs3(int* arr, int len, int value) {
-    int count = 0;
+    int len = arr.size();
+    int right, m1, m2, left;
     for (int i = 0; i < len; ++i) {
-        int index = binarySearch(arr, i + 1, len - 1, value - arr[i]);
-        count += index;
+        right = len - 1;
+        left = i + 1;
+        m2 = value - arr[i];
+        while (left <= right) {
+            m1 = (left + right) / 2;
+            if (arr[m1] == m2) {
+                count++;
+                int next = m1 - 1;
+                while ((next > i) && (arr[next] == arr[m1])) {
+                    count++;
+                    next--;
+                }
+                int next2 = m1 + 1;
+                while ((i < next2) && (arr[next2] == arr[m1])) {
+                    count++;
+                    next2++;
+                }
+                break;
+            } else if (arr[m1] > m2) {
+                right = m1 - 1;
+            } else {
+                left = m1 + 1;
+            }
+        }
     }
     return count;
 }
+
+int main() {
+    std::vector<int> arr = {1, 2, 3, 4, 5, 6};
+    int value = 7;
+
+    int pairsCount1 = countPairs1(arr, value);
+    int pairsCount2 = countPairs2(arr, value);
+    int pairsCount3 = countPairs3(arr, value);
+
+    std::cout << "Pairs count using countPairs1: " << pairsCount1 << std::endl;
+    std::cout << "Pairs count using countPairs2: " << pairsCount2 << std::endl;
+    std::cout << "Pairs count using countPairs3: " << pairsCount3 << std::endl;
+
+    return 0;
+}
+
