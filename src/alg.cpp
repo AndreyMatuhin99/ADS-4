66bbed185e3b00873c9f3d585fbeacd724705d95
AndreyMatuhin99 <116704246+AndreyMatuhin99@users.noreply.github.com>
2024-03-23 18:10:30 +0300

    Задание 4

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,95 +1,51 @@
 // Copyright 2021 NNTU-CS
-#include <iostream>
-#include <vector>
 #include <algorithm>
 
-int countPairs1(const std::vector<int>& arr, int value) {
+int countPairs1(int *arr, int len, int value) {
     int count = 0;
-    int len = arr.size();
-    for (int i = 0; i < (len - 1); i++) {
-        for (int j = i + 1; j < len; j++) {
-            if ((arr[i] + arr[j]) == value) {
-                count++;
+    for (int i = 0; i < len - 1; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                ++count;
             }
         }
     }
     return count;
 }
 
-int countPairs2(const std::vector<int>& arr, int value) {
-    int count = 0, x = 0, j = 0;
-    int len = arr.size();
-    for (int i = 0; i < (len - 1); i++) {
-        if ((arr[i] + arr[i + 1]) >= value) {
-            x = i;
-            break;
-        }
-    }
-    for (int i = 0; i < (len - 1); i++) {
-        if (arr[i] > value) {
-            break;
-        }
-        if (x <= i) {
-            j = i + 1;
+int countPairs2(int *arr, int len, int value) {
+    int count = 0;
+    int left = 0, right = len - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            ++count;
+            ++left;
+            --right;
+        } else if (sum < value) {
+            ++left;
         } else {
-            j = x;
-        }
-        for (; j < len; j++) {
-            if ((arr[i] + arr[j]) == value) {
-                count++;
-            }
-            if (arr[j] > value) {
-                break;
-            }
+            --right;
         }
     }
     return count;
 }
 
-int countPairs3(const std::vector<int>& arr, int value) {
+int countPairs3(int *arr, int len, int value) {
     int count = 0;
-    int len = arr.size();
-    int right, m1, m2, left;
     for (int i = 0; i < len; ++i) {
-        right = len - 1;
-        left = i + 1;
-        m2 = value - arr[i];
-        while (left <= right) {
-            m1 = (left + right) / 2;
-            if (arr[m1] == m2) {
-                count++;
-                int next = m1 - 1;
-                while ((next > i) && (arr[next] == arr[m1])) {
-                    count++;
-                    next--;
-                }
-                int next2 = m1 + 1;
-                while ((i < next2) && (arr[next2] == arr[m1])) {
-                    count++;
-                    next2++;
-                }
+        int low = i + 1, high = len - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[i] + arr[mid] == value) {
+                ++count;
                 break;
-            } else if (arr[m1] > m2) {
-                right = m1 - 1;
+            } else if (arr[i] + arr[mid] < value) {
+                low = mid + 1;
             } else {
-                left = m1 + 1;
+                high = mid - 1;
             }
         }
     }
     return count;
-}
-
-int main() {
-    std::vector<int> arr = {1, 2, 3, 4, 5, 6};
-    int value = 7;
-
-    int pairsCount1 = countPairs1(arr, value);
-    int pairsCount2 = countPairs2(arr, value);
-    int pairsCount3 = countPairs3(arr, value);
-
-    std::cout << "Pairs count using countPairs1: " << pairsCount1 << std::endl;
-    std::cout << "Pairs count using countPairs2: " << pairsCount2 << std::endl;
-    std::cout << "Pairs count using countPairs3: " << pairsCount3 << std::endl;
-
-    return 0;
 }
